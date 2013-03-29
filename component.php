b1bd5e8c950d3cb579c55b65e010c61b34a9e5cc
Johannes Hock <info@adhocgrafx.de>
2013-03-30 00:30:37 +0100

    print css

--- component.php
+++ component.php
@@ -11,20 +11,32 @@
 
 // no direct access
 defined('_JEXEC') or die;
+// get my params
+$logo = $this->params->get('logo');
+$sitetitle = $this->params->get('sitetitle');
 ?>
 <?php if (JRequest::getString('type')=='raw'):?>
 <jdoc:include type="component" />
 <?php else: ?>
+
 <!DOCTYPE html>
 <head>
 <jdoc:include type="head" />
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/base.css" type="text/css" media="screen, print" />
 <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/layout.css" type="text/css" media="screen, print" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/print.css" type="text/css" media="print" />
+<script src="http://use.edgefonts.net/source-sans-pro.js"></script>
+<script src="http://use.edgefonts.net/bitter.js"></script>
 </head>
 <body class="contentpane">
-<div class="headerlogo"> <a href="<?php echo $this->baseurl ?>" id="logo"> <IMG src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/images/logo.png" alt="joomskeleton"> </a> </div>
+<?php if ($logo): ?>
+	<div class="headerlogo"> <a href="<?php echo $this->baseurl ?>" id="logo" > <img src="<?php echo $this->baseurl ?>/<?php echo htmlspecialchars($logo); ?>"  alt="<?php echo htmlspecialchars($sitetitle); ?>" /> </a> </div>
+<?php else : ?>
+	<div class="headerlogo"> <a href="<?php echo $this->baseurl ?>" id="logo" > <IMG src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/images/logo.png" alt="JoomFluid 2.5" /> </a> </div>
+<?php endif;?>
 <jdoc:include type="message" />
 <jdoc:include type="component" />
+<div id="copy-pad"><p>JoomFluid 2.5 | adhocgraFX 2011 | &copy; alle Rechte vorbehalten</p></div>
 </body>
 </html>
 <?php endif; ?>
